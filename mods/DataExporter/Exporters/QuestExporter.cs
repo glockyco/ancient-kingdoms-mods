dc3c3cd25c053fa84c5e2b5e23bf009768b3d463
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-11-09 09:49:01 +0100

    refactor: use `idQuest` for quest predecessor values

--- mods/DataExporter/Exporters/QuestExporter.cs
+++ mods/DataExporter/Exporters/QuestExporter.cs
@@ -42,7 +42,7 @@ public class QuestExporter : BaseExporter
                 zone_id_quest_action = quest.idZoneQuestAction,
                 given_item_on_start_id = quest.givenItemOnStartQuest != null ? quest.givenItemOnStartQuest.name.ToLowerInvariant().Replace(" ", "_") : null,
                 predecessor_id = quest.predecessor != null && quest.predecessor.Length > 0 && quest.predecessor[0] != null
-                    ? quest.predecessor[0].name.ToLowerInvariant().Replace(" ", "_")
+                    ? quest.predecessor[0].idQuest
                     : null,
                 is_main_quest = quest.mainQuest,
                 is_epic_quest = quest.epicQuest,
