e7615fd8db5ec4e97dcb614620574f191f7a3fe0
Johann Glock <11704293+glockyco@users.noreply.github.com>
2026-04-29 17:59:10 +0200

    feat(bossmod): render ImGui demo window end-to-end as smoke test
    Wires ImGuiRenderer into MelonMod.OnGUI/OnDeinitializeMelon and renders
    ImGui.ShowDemoWindow as the v1 visibility test. UserData/BossMod/imgui.ini
    persists window state; UserData/BossMod/cache/cimgui.dll is the extracted
    native binary.
    
    API deviation: MelonUtils.UserDataDirectory is obsolete in this MelonLoader
    build; use MelonLoader.Utils.MelonEnvironment.UserDataDirectory.

--- mods/BossMod/BossMod.cs
+++ mods/BossMod/BossMod.cs
@@ -1,4 +1,8 @@
+using System.IO;
+using BossMod.Imgui;
+using ImGuiNET;
 using MelonLoader;
+using MelonLoader.Utils;
 
 [assembly: MelonInfo(typeof(BossMod.BossMod), "BossMod", "0.1.0", "ancient-kingdoms-mods")]
 [assembly: MelonGame("ancientpixels", "ancientkingdoms")]
@@ -7,8 +11,33 @@ namespace BossMod;
 
 public class BossMod : MelonMod
 {
+    private ImGuiRenderer _renderer;
+    private bool _showDemo = true;
+
     public override void OnInitializeMelon()
     {
-        LoggerInstance.Msg("BossMod initialized (skeleton)");
+        var userData = Path.Combine(MelonEnvironment.UserDataDirectory, "BossMod");
+        Directory.CreateDirectory(userData);
+        var iniPath = Path.Combine(userData, "imgui.ini");
+        var cacheDir = Path.Combine(userData, "cache");
+
+        _renderer = new ImGuiRenderer(LoggerInstance);
+        if (!_renderer.Init(iniPath, cacheDir))
+        {
+            LoggerInstance.Error("Renderer init failed; mod disabled");
+            _renderer = null;
+            return;
+        }
+
+        _renderer.OnLayout = () =>
+        {
+            if (_showDemo) ImGui.ShowDemoWindow(ref _showDemo);
+        };
+
+        LoggerInstance.Msg("BossMod initialized");
     }
+
+    public override void OnGUI() => _renderer?.OnGUI();
+
+    public override void OnDeinitializeMelon() => _renderer?.Dispose();
 }
