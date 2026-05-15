4500f6b5f7008ba6c9510bfa4443944091fb1d49
Johann Glock <11704293+glockyco@users.noreply.github.com>
2026-05-15 07:34:11 +0200

    chore(website): provision IndexNow key

---
+++ website/src/lib/seo/indexnow-key.ts
@@ -0,0 +1,6 @@
+/**
+ * IndexNow API key. Must match the filename of the key file under static/.
+ * IndexNow protocol: https://www.indexnow.org/documentation
+ */
+export const INDEXNOW_KEY = "b0384ee2818bf5cb2ce24f8699d2377f";
+export const INDEXNOW_KEY_LOCATION = `https://ancient-kingdoms.compendiums.org/${INDEXNOW_KEY}.txt`;
